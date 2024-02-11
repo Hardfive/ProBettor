c8c2f54282e3a9b1d65d3985a4e688652833c1f4
Hardfive <hhdur5132@gmail.com>
2024-02-11 15:41:02 +0000

    Init html templates

--- web_application/app/__init__.py
+++ web_application/app/__init__.py
@@ -1,14 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
-def create_app(test_config=None):
-    app = Flask(__name__)
+def create_app():
+    app = Flask(__name__, template_folder='./templates/')
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello World !'
+    @app.route('/')
+    def index():
+        return render_template('index.html')
     
-    from . import db
-    db.init_app(app)
+    @app.route('/underOver')
+    def underOver():
+        return render_template('overUnder.html')
     
     return app
